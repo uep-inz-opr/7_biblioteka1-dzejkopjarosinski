7858bc2e4c4c12dde4d239e2d20dce4350a7ce08
Jakub Jarosiński <dzejkopjarosinski@gmail.com>
2022-05-24 23:10:33 +0200

    first commit, seems to be fine

---
+++ main.py
@@ -0,0 +1,81 @@
+
+
+
+class Book:
+
+    def __init__(self, tytul, autor):
+        self.tytul = tytul
+        self.autor = autor
+
+class Egzemplarz:
+
+
+    def __init__(self, tytul, autor, rok):
+        self.tytul = tytul
+        self.autor = autor
+        self.rok = int(rok)
+
+
+        
+
+def dodaj_egzemplarz(tytul, autor, rok):
+    egzemplarz = Egzemplarz(tytul, autor, rok)
+    egzemplarze.append(egzemplarz)
+
+
+books = []
+egzemplarze = []
+book_amount = int(input())
+iterate = 0
+
+
+for book_input in range(book_amount):
+    book_input = eval(input())
+    tytul = book_input[0]
+    autor = book_input[1]
+    rok = book_input[2]
+    dodaj_egzemplarz(tytul, autor, rok)
+    book = Book(tytul, autor)
+    if iterate == 0:
+        books.append(book)
+        iterate += 1
+    else:
+        for ksiazka in books:
+            if (book.tytul != ksiazka.tytul) & (book.autor != ksiazka.autor):
+                books.append(ksiazka)
+                break
+            else:
+                continue
+
+
+for i in range(len(books)-1):
+    counter = 1
+    for y in range(i + 1, len(books)):
+        if books[i].autor == books[y].autor & books[i].tytul == books[y].tytul:
+            counter += 1
+
+        else:
+            continue
+    books[i].licznik = counter
+
+books[len(books)-1].licznik = 1
+no_copy = [None] * len(books)
+for i in range(len(books)):
+    no_copy[i] = books[i]
+
+
+for i in range(len(books)-1):
+    other = False
+    for y in range(i+1, len(books)):
+        if books[i].autor == books[y].autor & books[i].tytul == books[y].tytul:
+            if books[i].licznik > books[y].licznik:
+                no_copy.remove(books[y])
+
+a = []
+for ksiazka in no_copy:
+    appender = (ksiazka.tytul, ksiazka.autor, ksiazka.licznik)
+    a.append(appender)
+
+a = sorted(a, key=lambda x: x[0])
+for aappender in a:
+    print(appender)
